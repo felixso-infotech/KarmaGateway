b69cb45ae504ddebbbe7a506bad1e56016e04423
sarangibalu <sarangibalu.a@lxisoft.com>
2019-09-29 01:49:53 +0530

    KAR-232

--- src/main/java/com/lxisoft/client/karma_app/api/AggregateCommandResourceApiClient.java
+++
@@ -1,8 +0,0 @@
-package com.lxisoft.client.karma_app.api;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import com.lxisoft.client.karma_app.ClientConfiguration;
-
-@FeignClient(name="${KarmaApp.name:KarmaApp}", url="${KarmaApp.url:35.196.249.196:8075/KarmaApp}", configuration = ClientConfiguration.class)
-public interface AggregateCommandResourceApiClient extends AggregateCommandResourceApi {
-}
