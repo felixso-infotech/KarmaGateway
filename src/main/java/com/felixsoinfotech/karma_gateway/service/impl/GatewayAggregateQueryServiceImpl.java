f6ba814c2dedc893d212fdd6f60634db765b439f
muhammedruhail123 <muhammed.ruhail@lxisoft.com>
2019-12-26 12:35:46 +0530

    KAR-569

--- src/main/java/com/felixsoinfotech/karma_gateway/service/impl/GatewayAggregateQueryServiceImpl.java
+++ src/main/java/com/felixsoinfotech/karma_gateway/service/impl/GatewayAggregateQueryServiceImpl.java
@@ -33,57 +33,55 @@ import com.felixsoinfotech.karma_gateway.service.GatewayAggregateQueryService;
 @Service
 @Transactional
 public class GatewayAggregateQueryServiceImpl implements GatewayAggregateQueryService {
-	
+
 	/**
- 	 * Find time difference between current date and posted date.
- 	 *
- 	 * @param postedDateTime
- 	 *            to find the time
- 	 * 
- 	 * @return the time
- 	 */
+	 * Find time difference between current date and posted date.
+	 *
+	 * @param postedDateTime to find the time
+	 * 
+	 * @return the time
+	 */
 
- 	@Override
- 	public String calculateTimeDifferenceBetweenCurrentAndPostedTime(ZonedDateTime postedDateTime) {
- 		
- 		long offsetMillis = ZoneOffset.from(postedDateTime).getTotalSeconds() * 1000;
- 		long isoMillis = postedDateTime.toInstant().toEpochMilli();
- 		Date date = new Date(isoMillis + offsetMillis);
- 				
- 		Instant instant = Instant.now();
- 		long hours = 5;
- 		long minutes = 30;
- 		Instant instant1 = instant.plus(hours, ChronoUnit.HOURS).plus(minutes, ChronoUnit.MINUTES);
+	@Override
+	public String calculateTimeDifferenceBetweenCurrentAndPostedTime(ZonedDateTime postedDateTime) {
 
- 		Date current = Date.from(instant1);
- 		long diffInSecond = 0l;
- 		String diffInString = null;
- 		if (date != null) {
- 			diffInSecond = (current.getTime() - date.getTime()) / 1000l;
- 		}
- 		long postedBefore = 0l;
- 		if (diffInSecond < 60l) {
- 			diffInString = "just now";
- 		} else if (diffInSecond < 3600l) {
- 			postedBefore = diffInSecond / 60l;
- 			diffInString = postedBefore + " minutes ago";
- 		} else if (diffInSecond < 86400l) {
- 			postedBefore = diffInSecond / 3600l;
- 			diffInString = postedBefore + " hours ago";
- 		} else if (diffInSecond < 2592000l) {
- 			postedBefore = diffInSecond / 86400l;
- 			diffInString = postedBefore + " days ago";
- 		} else if (diffInSecond < 31104000l) {
- 			postedBefore = diffInSecond / 2592000l;
- 			diffInString = postedBefore + " months ago";
- 		} else {
- 			postedBefore = diffInSecond / 31104000l;
- 			diffInString = postedBefore + " years ago";
- 		}
+		System.out.println("*************" + postedDateTime);
+		long offsetMillis = ZoneOffset.from(postedDateTime).getTotalSeconds() * 1000;
+		long isoMillis = postedDateTime.toInstant().toEpochMilli();
+		Date date = new Date(isoMillis + offsetMillis);
 
- 		return diffInString;
- 	}
-    
+		Instant instant = Instant.now();
+		long hours = 5;
+		long minutes = 30;
+		Instant instant1 = instant.plus(hours, ChronoUnit.HOURS).plus(minutes, ChronoUnit.MINUTES);
 
+		Date current = Date.from(instant1);
+		long diffInSecond = 0l;
+		String diffInString = null;
+		if (date != null) {
+			diffInSecond = (current.getTime() - date.getTime()) / 1000l;
+		}
+		long postedBefore = 0l;
+		if (diffInSecond < 60l) {
+			diffInString = "just now";
+		} else if (diffInSecond < 3600l) {
+			postedBefore = diffInSecond / 60l;
+			diffInString = postedBefore + " minutes ago";
+		} else if (diffInSecond < 86400l) {
+			postedBefore = diffInSecond / 3600l;
+			diffInString = postedBefore + " hours ago";
+		} else if (diffInSecond < 2592000l) {
+			postedBefore = diffInSecond / 86400l;
+			diffInString = postedBefore + " days ago";
+		} else if (diffInSecond < 31104000l) {
+			postedBefore = diffInSecond / 2592000l;
+			diffInString = postedBefore + " months ago";
+		} else {
+			postedBefore = diffInSecond / 31104000l;
+			diffInString = postedBefore + " years ago";
+		}
+
+		return diffInString;
+	}
 
 }
